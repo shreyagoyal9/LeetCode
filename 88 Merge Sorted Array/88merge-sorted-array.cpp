54b1aca25c8002744615183653e0ab5dac5a71cf
shreyagoyal9 <shreyagoyal9988@gmail.com>
2025-09-19 18:04:16 +0000

    Code challenge submission for question: 88 Merge Sorted Array

---
+++ 88 Merge Sorted Array/88merge-sorted-array.cpp
@@ -0,0 +1,44 @@
+//optpimal solution approach 2 using shell sort gap method
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int len = m + n;
+        int gap = (len / 2) + (len % 2); //if 4.5 take 5 thatswhy
+
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+
+            while (right < len) {
+                // Case 1: both pointers in nums1
+                if (left < m && right < m) {
+                    if (nums1[left] > nums1[right]) {
+                        swap(nums1[left], nums1[right]);
+                    }
+                }
+                // Case 2: left in nums1, right in nums2
+                else if (left < m && right >= m) {
+                    if (nums1[left] > nums2[right - m]) {
+                        swap(nums1[left], nums2[right - m]);
+                    }
+                }
+                // Case 3: both in nums2
+                else {
+                    if (nums2[left - m] > nums2[right - m]) {
+                        swap(nums2[left - m], nums2[right - m]);
+                    }
+                }
+                left++;
+                right++;
+            }
+
+            if (gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
+        }
+
+        // Copy nums2 into nums1
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+    }
+};
