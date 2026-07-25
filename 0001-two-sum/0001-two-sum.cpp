18cfa29cc336606c928ea20d604aea430d101ac5
Shreya Goyal <shreyagoyal9988@gmail.com>
2026-07-25 22:39:01 +0530

    Time: 3 ms (66.53%), Space: 15 MB (21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,6 +2,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //putting the element and its index in hashmap, if not seen then proceed and put, if seen then return 
+        
         int n=nums.size();
         map<int,int>hashmap;
         for(int i=0;i<n;i++){
