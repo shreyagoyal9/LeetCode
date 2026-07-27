bd22ba27661db70bc1b9b65cb32478757ccaab9a
Shreya Goyal <shreyagoyal9988@gmail.com>
2026-07-27 11:04:21 +0530

    Time: 0 ms (100%), Space: 23.8 MB (56.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,20 +1,30 @@
-//brute force using another array space 
+//optimal 2 pointer approach 
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
-        vector<int>temp;
+        //put j on first 0 element's index, so set it to -1 if no 0 exists.
+        int j=-1;
+
+        //find index of first 0 and break (we got our j)
         for(int i=0;i<n;i++){
+            if (nums[i]==0){
+                j=i;
+                break; //the moment break executes entire for loop ends
+            }
+            
+        }
+        // If there is no zero, the array is already in the required form.
+         if(j == -1)
+             return;
+
+        //keep i just after j and if i is non zero swap i and j 
+        for(int i=j+1;i<n;i++){
             if(nums[i]!=0){
-                temp.push_back(nums[i]);
+                swap(nums[i],nums[j]);
+                j++;
             }
         }
-        for(int i=0;i<temp.size();i++){
-            nums[i]=temp[i];
-        }
-        for(int i=temp.size();i<n;i++){
-            nums[i]=0;
-        }
-        
     }
 };
